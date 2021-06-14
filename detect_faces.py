1d62ebd026ccc74329d69a520c09e1620ef7fc57
Ashish Yadav <ashudva@gmail.com>
2021-06-14 12:17:10 +0530

    File upload

---
+++ detect_faces.py
@@ -0,0 +1,28 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to input image")
+ap.add_argument("-p", "--prototxt", required=True,
+                help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+                help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(
+    cv2.resize(image, (300, 300)), 1.0,
+    (300, 300), (104.0, 177.0, 123.0)
+)
