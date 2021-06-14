fb956d5df819fb14b4c00003d965425f778cfd18
Ashish Yadav <ashudva@gmail.com>
2021-06-14 12:39:09 +0530

    Finish face_detection and plot bounding box

--- detect_faces.py
+++ detect_faces.py
@@ -2,6 +2,7 @@
 import numpy as np
 import argparse
 import cv2
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -15,14 +16,47 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
 args = vars(ap.parse_args())
 
 
-# load our serialized model from disk
+# load the serialized model from disk
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+
 # load the input image and construct an input blob for the image
 # by resizing to a fixed 300x300 pixels and then normalizing it
+print("[INFO] loading images and creating blob ...")
 image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
+# args : image, scale_factor/std, size, mean(R, G, B)
 blob = cv2.dnn.blobFromImage(
     cv2.resize(image, (300, 300)), 1.0,
     (300, 300), (104.0, 177.0, 123.0)
 )
+
+# setting input of NNet and getting object detection results
+print("[INFO] detecting objects...")
+net.setInput(blob)
+detections = net.forward()
+
+# loop over the detections and plot boxes over the faces
+for i in range(detections.shape[2]):
+    # get confidence for the i-th prediction
+    confidence = detections[0, 0, i, 2]
+    # fileter out weak predictions
+    if confidence > args["confidence"]:
+        # compute the coordinates for the bounding box
+        box = detections[0, 0, i, 3:7] * np.arry([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype('int')
+
+        # draw the bounding box
+        text = "{:.2f}".format(confidence * 100)
+        text = "{:.2f}%".format(confidence * 100)
+        y = (startY - 10) if (startY - 10) > 10 else (startY + 10)
+        cv2.rectangle(
+            image, (startX, startY),
+            (endX, endY), (0, 0, 255), 2)
+        cv2.putText(image, text, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
