c33dca7cce7059b98f0e65498bea103231c6238e
Ashish Yadav <ashudva@gmail.com>
2021-06-14 12:50:23 +0530

    Upload model prototxt and weights

--- detect_faces.py
+++ detect_faces.py
@@ -43,19 +43,20 @@ for i in range(detections.shape[2]):
     confidence = detections[0, 0, i, 2]
     # fileter out weak predictions
     if confidence > args["confidence"]:
-        # compute the coordinates for the bounding box
-        box = detections[0, 0, i, 3:7] * np.arry([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype('int')
+        continue
+    # compute the coordinates for the bounding box
+    box = detections[0, 0, i, 3:7] * np.arry([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype('int')
 
-        # draw the bounding box
-        text = "{:.2f}".format(confidence * 100)
-        text = "{:.2f}%".format(confidence * 100)
-        y = (startY - 10) if (startY - 10) > 10 else (startY + 10)
-        cv2.rectangle(
-            image, (startX, startY),
-            (endX, endY), (0, 0, 255), 2)
-        cv2.putText(image, text, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    # draw the bounding box
+    text = "{:.2f}".format(confidence * 100)
+    text = "{:.2f}%".format(confidence * 100)
+    y = (startY - 10) if (startY - 10) > 10 else (startY + 10)
+    cv2.rectangle(
+        image, (startX, startY),
+        (endX, endY), (0, 0, 255), 2)
+    cv2.putText(image, text, (startX, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
 # show the output image
 cv2.imshow("Output", image)
